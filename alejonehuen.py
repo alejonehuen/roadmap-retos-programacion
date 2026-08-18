91e24042412e117e82f5fca84bf9098d2d4ba111
alejonehuen <alejonehuen@gmail.com>
2026-08-17 22:57:41 -0300

    Add files via upload

---
+++ alejonehuen.py
@@ -0,0 +1,19 @@
+#Hola mundo estoy usando Python: https://www.python.org/
+
+"Esto es un comentario en una línea"
+
+"""Esto es 
+un comentarios
+en varias 
+líneas"""
+
+
+"hola soy Alejo" "estoy usando Python" #Cadena de texto
+
+0, 1, 2, 3, 4, 5 #Enteros
+
+True #Booleanos
+False #Booleanos
+
+
+print ("¡Hola Python!")
